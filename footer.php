c9a3c44c4dfc1cca08bcd51b7d2ecb54c20034bd
mohamed abdelrahman <131889931+m7amh@users.noreply.github.com>
2025-05-21 23:05:17 +0300

    Delete footer.php

--- footer.php
+++
@@ -1,9 +0,0 @@
-<footer class="footer">
-    <div class="container">
-        <p>&copy; <?php echo date('Y'); ?> Auth System. All rights reserved.
-        &copy; M7M7 team.
-        </p>
-        </div>
-</footer>
-</body>
-</html>
